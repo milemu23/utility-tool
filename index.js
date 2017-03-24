05a52364231ee47a4b8f049b4e116638766d187f
milemu23 <design@michellemunksgard.com>
2017-03-23 22:14:33 -0400

    Update index.js
    Added a separator so when output shows in console it's more noticeable. Removed option to send logs to a file.

--- index.js
+++ index.js
@@ -2,25 +2,23 @@
 
 // Use Colors Package
 const colors = require('colors');
-const fs = require('fs');
 
 // console.log(colors.green(message));
-function debug(info, status) {
+function debug(title, info, status) {
   if (process.env.DEBUG === 'true') {
-    if (status === 'success') {
-      console.log(colors.green(info));
-    } else if (status === 'error') {
-      console.log(colors.red(info));
-    }
-
+  // added a separator
+    const sep = '\n*********DEBUG*********\n';
+    const out = sep + title + sep;
     const date = new Date();
     const timestamp = '[' + date.toISOString() + '] ';
 
-    fs.appendFile('src/logs/log.log', '\n' + timestamp + info, (err) => {
-      if (err !== null) { // If there is an error, write the error to the log.
-        fs.appendFile('src/logs/log.log', '\n' + err);
-      }
-    });
+    if (status === 'success') {
+      console.log(colors.green(out, 'Success', info, timestamp));
+    } else if (status === 'error') {
+      console.log(colors.red(out, 'Error', info, timestamp));
+    }
+
+
   }
 }
 
